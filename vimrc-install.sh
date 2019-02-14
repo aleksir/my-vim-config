0fe7dbad82de0bafa3ec4dff3bef29951131a8a1
Aleksi Rautakoski <aleksi.rautakoski@aitiofinland.com>
2019-02-14 09:38:28 +0200

    fixed install script

--- vimrc-install.sh
+++ vimrc-install.sh
@@ -17,8 +17,8 @@ fi
 if [ -e $HOME/.vimrc ]; then mv $HOME/.vimrc{,_bak}; fi
 if [ -e $HOME/.gvimrc ]; then mv $HOME/.gvimrc{,_bak}; fi
 
-ln -sf vimrc $HOME/.vimrc
-ln -sf gvimrc $HOME/.gvimrc
+ln -sf $PWD/vimrc $HOME/.vimrc
+ln -sf $PWD/gvimrc $HOME/.gvimrc
 
 if [ ! -e $HOME/.vim/autoload/plug.vim ]; then
     curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
